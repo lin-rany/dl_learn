a92d62b78f328a77abd9a896d44515cbb1750ee5
lin_rany <lin_rany@qq.com>
2024-08-25 03:45:43 +0800

    add transfrom

---
+++ download.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Initialize our own variables
+model_name=""
+dataset_name=""
+export HF_ENDPOINT=https://hf-mirror.com
+# Parsing options
+while getopts ":m:d:" opt; do
+  case $opt in
+    m)
+      model_name="$OPTARG"
+      ;;
+    d)
+      dataset_name="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option -$OPTARG" >&2
+      ;;
+  esac
+done
+
+# Shift off the options and optional --.
+shift $((OPTIND-1))
+
+# Check positional arguments
+if [ -n "$1" ]; then
+  model_name="$1"
+fi
+
+if [ -n "$2" ]; then
+  dataset_name="$2"
+fi
+
+# Display the variables
+# echo "Model name: $model_name"
+# echo "Dataset name: $dataset_name"
+# =$1
+
+if [ ${#model_name} -gt 0 ]
+then
+  echo "Model name: $model_name"
+  huggingface-cli download --resume-download ${model_name} --local-dir model/${model_name}
+else
+  echo "Model name is not set or empty"
+fi
+
+if [ ${#dataset_name} -gt 0 ]
+then
+  echo "Dataset name: $dataset_name"
+  huggingface-cli download --repo-type dataset --resume-download ${dataset_name} --local-dir dataset/${dataset_name}
+else
+  echo "Dataset name is not set or empty"
+fi
+# if $model_name
+#
